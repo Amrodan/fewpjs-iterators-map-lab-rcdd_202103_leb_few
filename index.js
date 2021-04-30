83f748a98075d84e120cbd09f93c7b7ed66db7eb
Amro dandashli <amrdandashli@gmail.com>
2021-04-30 01:39:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,20 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+<<<<<<< HEAD
 const titleCased = () => tutorials.map(tutorial => tutorial.split(" ").map(
    word => word[0].toUpperCase() + word.slice(1)
 ).join(' ')
 
 )
  
+=======
+// function titleCase(str) {
+//   let titled = tutorials.toLowerCase().split(' ').map(function(elem) {
+//     return elem[0].toUpperCase()+ elem.slice(1);
+//     });return titled.join(' '); }
+    const titleCased = tutorials.map(str) => { tutorials.toLowerCase().split(' ')
+      return str[0].toUpperCase() + str.silce(1);
+      return tutorials.joine(' ')
+    }
+>>>>>>> 689320d4cfa449c396f94e7b4f0c448bd6cd10ac
